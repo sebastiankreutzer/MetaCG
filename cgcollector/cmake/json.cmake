988720590ff50b5b0a0f28ffc697a1788b4efdaa
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2021-10-19 12:55:56 +0200

    Adjust json cmake

--- cgcollector/cmake/json.cmake
+++ cgcollector/cmake/json.cmake
@@ -1,13 +1,7 @@
 include(ExternalProject)
 
 if(DEFINED JSON_INCLUDE)
-  message("JSON_INCLUDE predefined: ${JSON_INCLUDE}")
-else()
-  find_path(JSON_LIBRARY NAMES json)
-  if(JSON_LIBRARY)
-    set(JSON_INCLUDE ${JSON_LIBRARY}/json/single_include)
-    message("JSON found in ${JSON_INCLUDE}")
-  else()
+
     message("JSON library not found, download into extern during make")
     ExternalProject_Add(json
       SOURCE_DIR          ${CMAKE_CURRENT_SOURCE_DIR}/extern/json
@@ -20,7 +14,7 @@ else()
       GIT_SHALLOW         true
     )
     set(JSON_INCLUDE ${CMAKE_CURRENT_SOURCE_DIR}/extern/json/single_include)
-  endif()
+
 endif()
 
 function(add_json target)

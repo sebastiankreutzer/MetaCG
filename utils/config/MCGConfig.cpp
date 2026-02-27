747ee9d489e52b9c4eee05632c751aa99726c9e2
Sebastian Kreutzer <sebastian.kreutzer@tu-darmstadt.de>
2026-02-27 16:24:59 +0100

    Fix MCGConfig

--- utils/config/MCGConfig.cpp
+++ utils/config/MCGConfig.cpp
@@ -14,12 +14,32 @@
 #define CAGE_PLUGIN
 #endif
 
+static std::filesystem::path getRootPath() {
+  auto exe = std::filesystem::canonical("/proc/self/exe");
+#ifdef BUILD_TREE
+  auto prefix = exe.parent_path().parent_path().parent_path();
+#else
+  auto prefix = exe.parent_path().parent_path();
+#endif
+  return prefix;
+}
+
+static std::filesystem::path getCaGePlugin(const std::filesystem::path& prefix) {
+#ifdef BUILD_TREE
+  auto path =  prefix / "tools/cage/src/";
+#else
+    auto path =  prefix / INSTALL_LIBDIR;
+#endif
+  return path / CAGE_PLUGIN;
+}
+
 int main(int argc, char** argv) {
   cxxopts::Options options("metacg-config", "MetaCG configuration tool");
   // clang-format off
   options.add_options("commands")("v,version", "Prints the version of this MetaCG installation.")(
       "revision", "Prints the revision hash of this MetaCG installation")(
       "prefix", "Prints the installation prefix of this MetaCG installation.")(
+      "libdir", "Prints the library path of this MetaCG installation.")(
       "cage-ldflags", "Prints the necessary ld flags.")(
       "cage-cflags", "Prints the necessary C compile flags.")(
       "cage-cxxflags", "Prints the necessary C++ compile flags.")(
@@ -45,13 +65,15 @@ int main(int argc, char** argv) {
 #endif
 
   // Exactly one of these is allowed at the same time
-  int optCount = result.count("version") + result.count("revision") + result.count("prefix") + (cageOptionSet ? 1 : 0);
+  int optCount = result.count("version") + result.count("revision") + result.count("prefix") + result.count("libdir") + (cageOptionSet ? 1 : 0);
   if (optCount == 0) {
     std::cerr << "Error: No command specified.\n";
     return EXIT_FAILURE;
   } else if (optCount > 1) {
     std::cerr << "Warning: Multiple mutually exclusive commands specified. Only one of them will be processed.\n";
   }
+
+  auto prefix = getRootPath();
 
   if (result.contains("version")) {
     std::cout << MetaCG_VERSION_MAJOR << "." << MetaCG_VERSION_MINOR;
@@ -64,7 +86,12 @@ int main(int argc, char** argv) {
   }
 
   if (result.contains("prefix")) {
-    std::cout << INSTALL_PREFIX;
+    std::cout << prefix.string();
+    return EXIT_SUCCESS;
+  }
+
+  if (result.contains("libdir")) {
+    std::cout << (prefix / INSTALL_LIBDIR).string();
     return EXIT_SUCCESS;
   }
 
@@ -86,8 +113,9 @@ int main(int argc, char** argv) {
     std::cout << " -flto ";
   }
 
+  auto cagePlugin = getCaGePlugin(prefix).string();
   if (result.contains("cage-ldflags")) {
-    std::cout << " -flto -fuse-ld=lld -Wl,-mllvm=-load=" << CAGE_PLUGIN << " -Wl,--load-pass-plugin=" << CAGE_PLUGIN
+    std::cout << " -flto -fuse-ld=lld -Wl,-mllvm=-load=" << cagePlugin << " -Wl,--load-pass-plugin=" << cagePlugin
               << processedPassOpts << " ";
   }
 #endif
